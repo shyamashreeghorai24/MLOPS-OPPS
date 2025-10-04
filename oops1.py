5aba289142dcf5fe09f8dbce97a260283ba5d5e4
shyamashreeghorai24 <aparnaghorai17@gmail.com>
2025-10-04 21:15:00 +0530

    classes and objects

---
+++ oops1.py
@@ -0,0 +1,21 @@
+#iniriate a class
+class employee:
+    # special methos/magic method - constructor
+    def __init__(self):
+        print("Started executing attributes/data")
+        self.id = 123
+        self.salary = 5000
+        self.designation = "SDE"
+        print("attributes/data have been initiated")
+
+    def travel(self,destination):
+        print("This travel function was called manually")
+        print(f"Employeeis now travelling to {destination}")    
+
+# creating an obj/instance of the class
+sam = employee()     
+# print(sam.id)
+# print(sam.salary)
+# print(sam.designation)   
+print(sam.travel("kerala"))
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,10 @@
+lst = [123]
+my_str = "mlops playlist"
+my_int = 155
+# print(type(my_int))
+# print(lst.capitalize())
+# my_str = my_str.capitalize()
+# print(lst)
+a ='x'
+b='y'
+print(a+b)
